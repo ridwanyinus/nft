e4c0a093675f9e3704803d64e3a1aa57970fbca8
Ridwan <ridwanyinusa15@gmail.com>
2024-07-22 19:10:57 +0100

    added changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="bg-dark relative w-full h-full overflow-hidden  md:px-[1.9rem] xl:px-10 desktop:px-12 largesceen:px-36 fourk:px-60">
-      <section>
+      <section className="relative">
+        <div className="section-bg w-full h-full"></div>
+
         <Navbar />
       </section>
     </main>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
-    <nav className="pt-6 lg:pt-8 px-5   w-full  pb-14 lg:pb-20 relative z-10 flex gap-x-10 bg-green-900">
+    <nav className="pt-6 lg:pt-8 px-5   w-full  pb-14 lg:pb-20 relative z-10 flex gap-x-10  *:text-white">
       <Link href="/" className="desktop:flex-1  text-[1.75rem] font-bold leading-[118.5%] logo relative z-[999]">
         KNFT.
       </Link>
