01efba868afd16a059cbae7c5e6a243c02215f5b
Ridwan <ridwanyinusa15@gmail.com>
2024-07-22 18:48:57 +0100

    added changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,25 @@
 import type { Config } from "tailwindcss";
-
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      screens: {
+        xs: "375px",
+        small: "470px",
+        medium: "730px",
+        desktop: "1365px",
+        largesceen: "1700px",
+        fourk: "2400px",
+      },
+      colors: {
+        dark: "#04011C",
+        pink: "#E50AA7",
+        cyan: "#1EE0E1",
+        blue: "#07003E",
+      },
+
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "hero-pattern": "url('../public/Union.png')",
       },
     },
   },
