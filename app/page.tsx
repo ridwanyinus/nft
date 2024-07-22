4205ecb80af8a21685994f07254f62150cd88a16
Ridwan <ridwanyinusa15@gmail.com>
2024-07-22 19:06:51 +0100

    added changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
-      <Navbar />
+    <main className="bg-dark relative w-full h-full overflow-hidden  md:px-[1.9rem] xl:px-10 desktop:px-12 largesceen:px-36 fourk:px-60">
+      <section>
+        <Navbar />
+      </section>
     </main>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,6 +5,8 @@ import { IoClose } from "react-icons/io5";
 import { navItems } from "@/Data/data";
 import Link from "next/link";
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <nav className="pt-6 lg:pt-8 px-5   w-full  pb-14 lg:pb-20 relative z-10 flex gap-x-10 bg-green-900">
       <Link href="/" className="desktop:flex-1  text-[1.75rem] font-bold leading-[118.5%] logo relative z-[999]">
