0a6f5b1d9ba53eea06c47b7430ff87ee4230c765
Ridwan <ridwanyinusa15@gmail.com>
2024-07-22 18:44:31 +0100

    added changes

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+const Navbar = () => {
+  return (
+    <nav className="flex gap-x-4 *:text-green-900 bg-slate-600">
+      <Link href="/">add</Link>
+      <Link href="/">add</Link>
+      <Link href="/">add</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
