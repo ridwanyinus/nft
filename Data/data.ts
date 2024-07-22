dada9a76dbfe5fab03e97a34bb138d9d66398a90
Ridwan <ridwanyinusa15@gmail.com>
2024-07-22 19:03:32 +0100

    added changes

---
+++ Data/data.ts
@@ -0,0 +1,12 @@
+export type NavItem = {
+  name: string;
+  link: string;
+};
+
+export const navItems: NavItem[] = [
+  { name: "Home", link: "/" },
+  { name: "All NFTs", link: "#nFTs" },
+  { name: "Team", link: "#team" },
+  { name: "Terms & Co.", link: "#terms" },
+  { name: "Contact", link: "#contact" },
+];
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,15 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineAlignLeft } from "react-icons/ai";
+import { IoClose } from "react-icons/io5";
+import { navItems } from "@/Data/data";
 import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="pt-6 lg:pt-8 px-5   w-full  pb-14 lg:pb-20 relative z-10 flex gap-x-10 bg-green-900">
+      <Link href="/" className="desktop:flex-1  text-[1.75rem] font-bold leading-[118.5%] logo relative z-[999]">
+        KNFT.
+      </Link>
       <Link href="/">add</Link>
       <Link href="/">add</Link>
       <Link href="/">add</Link>
