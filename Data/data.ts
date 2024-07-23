bc3e019975329b823c9841301cb2f7d8dc9882d6
Ridwan <ridwanyinusa15@gmail.com>
2024-07-23 11:23:55 +0100

    added changes

--- Data/data.ts
+++ Data/data.ts
@@ -1,3 +1,14 @@
+import Image from "next/image";
+import image1 from "@/public/image 8.png";
+import image2 from "@/public/image 10.png";
+import image3 from "@/public/image 7.png";
+import image4 from "@/public/image 11.png";
+import image5 from "@/public/image 12.png";
+import image6 from "@/public/image 13.png";
+import eth from "@/public/Ethereum.png";
+import star1 from "@/public/Christmas Star.png";
+import star2 from "@/public/Christmas Star-black.png";
+
 export type NavItem = {
   name: string;
   link: string;
@@ -16,4 +27,55 @@ export const Overiew = [
   { name: "Digital Artists", stats: "17.000 +" },
   { name: "Live Auctions", stats: "22.000 +" },
   { name: "Unique Products", stats: "50.000 +" },
+];
+
+export const Nfts = [
+  {
+    name: "Cyber Samurai #575",
+    Eth: "8.5",
+    img: image1,
+    ethImg: eth,
+    star: star1,
+    starBlack: star2,
+  },
+  {
+    name: "Cyber Samurai #784",
+    Eth: "7.5",
+    img: image2,
+    ethImg: eth,
+    star: star1,
+    starBlack: star2,
+  },
+  {
+    name: "Cyber Samurai #475",
+    Eth: "15.5",
+    img: image3,
+    ethImg: eth,
+    star: star1,
+    starBlack: star2,
+  },
+  {
+    name: "Cyber Samurai #475",
+    Eth: "7.5",
+    img: image4,
+    ethImg: eth,
+    star: star1,
+    starBlack: star2,
+  },
+  {
+    name: "Cyber Samurai #475",
+    Eth: "7.5",
+    img: image5,
+    ethImg: eth,
+    star: star1,
+    starBlack: star2,
+  },
+  {
+    name: "Cyber Samurai #475",
+    Eth: "15.5",
+    img: image6,
+    ethImg: eth,
+    star: star1,
+    starBlack: star2,
+  },
 ];
--- components/PopularNfts.tsx
+++ components/PopularNfts.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const PopularNfts = () => {
-  return <main>PopularNfts</main>;
+  return (
+    <main className="py-10  sm:py-14  lg:py-20  2xl:py-24 px-5 lg:px-0">
+      <h1 className="text-white font-black leading-normal text-2xl sm:text-3xl md:text-[2.5rem] largesceen:text-5xl text-center">
+        <span className="italic font-light text-cyan ">Most Popular</span> NFTs
+      </h1>
+    </main>
+  );
 };
 
 export default PopularNfts;
