111f50c1ce3a94dd41fc932648533794c232c827
Ridwan <ridwanyinusa15@gmail.com>
2024-07-23 16:56:42 +0100

    added changes

--- components/PopularNfts.tsx
+++ components/PopularNfts.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import h1Group from "@/public/Group 47474.svg";
 import Image from "next/image";
 import { Nfts } from "@/Data/data";
+import { FaHeart } from "react-icons/fa6";
+
 const PopularNfts = () => {
   return (
     <main className="py-10  sm:py-14  lg:py-20  2xl:py-24 px-5 lg:px-0">
@@ -24,12 +26,16 @@ const PopularNfts = () => {
       <section className="grid grid-cols-2 sm:grid-cols-3 fourk:grid-cols-6 gap-y-6 sm:gap-y-8 md:gap-y-12 gap-x-3 sm:gap-x-4 md:gap-x-6 lg:gap-x-12 xl:gap-x-10 desktop:gap-x-14 largesceen:gap-x-28 fourk:gap-x-8">
         {Nfts.map((items, idx: number) => (
           <div key={idx} className="">
-            <div className="card-bg w-fit p-3  md:p-4 lg:p-6 desktop:p-8">
+            <div className="card-bg w-fit p-3  md:p-4 lg:p-6 desktop:p-8 relative">
               <Image
                 src={items.img}
                 alt="nft"
                 className="max-md:object-cover w-[10rem] h-[10rem] sm:w-[21.313rem] sm:h-[12rem] md:h-[15.313rem] lg:h-[19rem] xl:h-[21.201rem] largesceen:w-[23rem] mb-4 md:mb-6 rounded-xl md:rounded-2xl"
               />
+
+              <div className="heart p-1 md:p-2 desktop:p-3 w-fit absolute top-4 right-4 md:top-8 md:right-6 lg:top-10 desktop:top-12 lg:right-8 desktop:right-12">
+                <FaHeart className=" text-white w-3 h-3  lg:w-4 lg:h-4 desktop:w-6 desktop:h-6 active:text-orange-600" />
+              </div>
 
               <div className="flex  xs:space-x-4 sm:space-x-3 md:space-x-6 lg:space-x-6 xl:space-x-8 desktop:space-x-10 2xl:space-x-12 mb-1 sm:mb-2 items-center">
                 <h4 className="text-white font-semibold text-[11px] md:text-xs lg:text-base xl:text-xl desktop:text-[1.375rem] largesceen:text-2xl ">{items.name}</h4>
@@ -39,7 +45,6 @@ const PopularNfts = () => {
                   <h4 className="text-white font-semibold text-[11px] md:text-xs lg:text-base xl:text-xl desktop:text-[1.375rem] largesceen:text-2xl">{items.Eth}</h4>
                 </div>
               </div>
-
               <div className="flex">
                 <Image src={items.star} alt="star" className="w-3 md:w-[1.125rem] h-auto " />
                 <Image src={items.star} alt="star" className="w-3 md:w-[1.125rem] h-auto " />
@@ -47,7 +52,6 @@ const PopularNfts = () => {
                 <Image src={items.starBlack} alt="star" className="w-3 md:w-[1.125rem] h-auto " />
                 <Image src={items.starBlack} alt="star" className="w-3 md:w-[1.125rem] h-auto " />
               </div>
-
               <button
                 type="button"
                 className="mt-4 md:mt-6 lg:mt-8 text-white text-xs md:text-sm lg:text-base max-md:rounded-md xl:text-xl py-1 md:py-2 xl:py-3   font-bold font-helvetica hover:text-cyan transition-colors btn-1 leading-[150%] w-full relative cursor-pointer">
