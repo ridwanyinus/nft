2aa5c4123a13060f1a60361d4c1fe307cd83ec0f
Ridwan <ridwanyinusa15@gmail.com>
2024-07-27 14:41:17 +0100

    added changes

--- components/CardComponent.tsx
+++ components/CardComponent.tsx
@@ -1,4 +1,3 @@
-// CardComponent.tsx
 "use client";
 import React from "react";
 import Image, { StaticImageData } from "next/image";
@@ -8,7 +7,7 @@ interface CardComponentProps {
 }
 
 const CardComponent: React.FC<CardComponentProps> = ({ img }) => {
-  return <Image src={img} alt="companies" className="object-contain w-8 h-6 sm:w-12 sm:h-6 md:w-12 md:h-7 lg:w-16 lg:h-8 xl:w-20 xl:h-9 desktop:w-full desktop:h-12" />;
+  return <Image src={img} alt="companies" className="object-contain w-12 h-8 sm:w-12 sm:h-6 md:w-12 md:h-7 lg:w-16 lg:h-8 xl:w-20 xl:h-9 desktop:w-full desktop:h-12" />;
 };
 
 export default CardComponent;
